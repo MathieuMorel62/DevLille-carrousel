8fe99ba76f5113a9fb22e4bf55ba27d2f769667b
Mathieu Morel <mathieu.morel@holbertonschool.com>
2026-06-24 13:03:36 +0200

    Add profiles data for users including personal details, tech stacks, and social links

---
+++ src/data/profiles.js
@@ -0,0 +1,323 @@
+export const PROFILES = [
+  {
+    name: "Guillaume V.",
+    accent: "#ff2e1f",
+    search: "Alternance",
+    role: "Cybersécurité / DevOps",
+    rhythm: "4 jours. / 1 jour.",
+    available: "sept. 2026 / oct. 2026",
+    avatar: "/avatars/vaudet_guillaume.jpg",
+    stack: [
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "Bash", icon: "devicon-bash-plain" },
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Php", icon: "devicon-php-plain" },
+      { name: "Javascript", icon: "devicon-javascript-plain" },
+      { name: "React", icon: "devicon-react-plain" },
+      { name: "Jira", icon: "devicon-jira-plain" },
+      { name: "Ansible", icon: "devicon-ansible-plain" },
+      { name: "Terraform", icon: "devicon-terraform-plain" },
+      { name: "Git", icon: "devicon-git-plain" },
+      { name: "Docker", icon: "devicon-docker-plain" },
+      { name: "AWS", icon: "devicon-amazonwebservices-plain-wordmark" },
+      { name: "Prometheus", icon: "devicon-prometheus-original" },
+    ],
+    quote: "Tout est exploitable. Surtout la pause café.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/guillaume-vaudet/",
+      github: "https://github.com/GuillaumeVaudet",
+    },
+  },
+  {
+    name: "Maxime B.",           // ton prénom + initiale du nom
+    accent: "#ff6a00",           // couleur de ton profil (voir palette ci-dessous)
+    search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Dev Fullstack",       // ton intitulé de poste
+    rhythm: "4 jours. / 1 jour.",   // rythme d'alternance (ou "" si pas concerné)
+    available: "déc. 2026",     // date de disponibilité
+    avatar: "/avatars/bernier_maxime.jpg",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "JavaScript", icon: "devicon-javascript-plain" },
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "Java", icon: "devicon-java-plain-wordmark" },
+      { name: "MySQL", icon: "devicon-mysql-plain" },
+      { name: "HTML5", icon: "devicon-html5-plain" },
+      { name: "CSS3", icon: "devicon-css3-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Bash", icon: "devicon-bash-plain" },
+      { name: "Git", icon: "devicon-git-plain" }
+
+      // ajoute autant de technos que tu veux
+    ],
+    quote: "Faire du code et coder de la musique... ou l'inverse.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/maxime-bernier-dev/",
+      github: "https://github.com/Litoxam",
+    },
+  },
+  {
+    name: "Elliot C.",           // ton prénom + initiale du nom
+    accent: "#ff6a00",           // couleur de ton profil (voir palette ci-dessous)
+    search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Developpeur Machine Learning",       // ton intitulé de poste
+    rhythm: "4j / 1j",   // rythme d'alternance (ou "" si pas concerné)
+    available: "aout/septembre ou novembre 2026",     // date de disponibilité
+    avatar: "/avatars/Elliot_CHARLET.jpg",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "JavaScript", icon: "devicon-javascript-plain" },
+      { name: "MySQL", icon: "devicon-mysql-plain" },
+      { name: "MongoDB", icon: "devicon-mongodb-plain" },
+      { name: "CSS", icon: "devicon-css3-plain" },
+      { name: "HTML", icon: "devicon-html5-plain" },
+      { name: "Git", icon: "devicon-git-plain" },
+      { name: "Docker", icon: "devicon-docker-plain" },
+    ],
+    quote: "Si t'as 2 minutes n'hesites pas à ce qu'on discute bases de données et bases vectorielles",
+    links: {
+      linkedin: "https://www.linkedin.com/in/elliot-charlet/",
+      github: "https://github.com/aragoza",
+    },
+  },
+  {
+    name: "Rawan S.",           // ton prénom + initiale du nom
+    accent: "#f5a623",           // couleur de ton profil (voir palette ci-dessous)
+    search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Développeur Fullstack",       // ton intitulé de poste
+    rhythm: "4j / 1j",   // rythme d'alternance (ou "" si pas concerné)
+    available: "décembre 2026",     // date de disponibilité
+    avatar: "/avatars/rawan_safouan.jpg",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "Git", icon: "devicon-git-plain" },
+      { name: "MySQL", icon: "devicon-mysql-plain" },
+      { name: "Bash", icon: "devicon-bash-plain" },
+
+    ],
+    quote: "Je réponds dès que ce bug cesse d'être plus intelligent que moi.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/teima-rawan-safouan-7502442b1",
+      github: "https://github.com/rawan-holberton",
+},
+},
+{    name: "Christophe S.",           // ton prénom + initiale du nom
+    accent: "#ff6a00",           // couleur de ton profil (voir palette ci-dessous)
+    search: "CDD, CDI",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Dev Back-end, Android",       // ton intitulé de poste   // rythme d'alternance (ou "" si pas concerné)
+    available: "Now",     // date de disponibilité
+    avatar: "/avatars/christophe_saniez.jpg",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "Flutter", icon: "devicon-flutter-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "JavaScript", icon: "devicon-javascript-plain" },
+      { name: "MySQL", icon: "devicon-mysql-plain" },
+      { name: "Supabase", icon: "devicon-postgresql-plain" },
+      { name: "CSS", icon: "devicon-css3-plain" },
+      { name: "HTML", icon: "devicon-html5-plain" },
+      { name: "Git", icon: "devicon-git-plain" },
+      { name: "Docker", icon: "devicon-docker-plain" },
+    ],
+    quote: "Développeur passionné formé aux fondamentaux du logiciel, j'apporte ma maturité professionnelle et ma rigueur au service de vos applications.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/saniez-lenthieul-christophe/",
+      github: "https://github.com/Saniez-l",
+    },
+  },
+{
+  name: "Léo L.",           // ton prénom + initiale du nom
+  accent: "#f5a623",           // couleur de ton profil (voir palette ci-dessous)
+  search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+  role: "Machine Learning",       // ton intitulé de poste
+  rhythm: "4 jours. / 1 jour.",   // rythme d'alternance (ou "" si pas concerné)
+  available: "déc. 2026",     // date de disponibilité
+  avatar: "/avatars/leo_lebtahi.jpg",  // chemin vers ta photo (étape 2)
+  stack: [
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "C",   icon: "devicon-c-plain" },
+      { name: "Python",  icon: "devicon-python-plain" },
+      { name: "JavaScript",  icon: "devicon-javascript-plain" },
+      { name: "Docker",  icon: "devicon-docker-plain" },
+      { name: "HTML5",  icon: "devicon-html5-plain" },
+      { name: "CSS3",  icon: "devicon-css3-plain" },
+      { name: "Linux",  icon: "devicon-linux-plain" },
+      { name: "Bash",  icon: "devicon-bash-plain" },
+      { name: "Git",  icon: "devicon-git-plain" },
+      { name: "MySQL",  icon: "devicon-mysql-plain" },
+
+
+    // ajoute autant de technos que tu veux
+  ],
+  quote: "Bien comprendre son data, le défi d'une bonne entreprise.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/lebtahi-léo-902596309",
+    github: "https://github.com/Lunaruo",
+  },
+},
+{
+  name: "Ulysse D.",
+  accent: "#ff4500",
+  search: "Stage, Alternance",
+  role: "Cybersécurité",
+  rhythm: "4 jours / 1 jour.",
+  available: "sept. 2026",
+  avatar: "/avatars/ulysse_dewaleyne.jpg",
+  stack: [
+      { name: "C",   icon: "devicon-c-plain" },
+      { name: "Bash",  icon: "devicon-bash-plain" },
+      { name: "Python",  icon: "devicon-python-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Git",  icon: "devicon-git-plain" },
+      { name: "MySQL",  icon: "devicon-mysql-plain" },
+      { name: "Docker",  icon: "devicon-docker-plain" },
+  ],
+  quote: "Je parle chinois mais je ne comprends plus mes noms de variables.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/udewaleynepro/",
+    github: "https://github.com/udcyber",
+  },
+},
+  {
+  name: "Robin B.",           // ton prénom + initiale du nom
+  accent: "#ff1100",           // couleur de ton profil (voir palette ci-dessous)
+  search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+  role: "CYBERSÉCURITÉ/DEVOPS",       // ton intitulé de poste
+  rhythm: "4 jours / 1 jour.",   // rythme d'alternance (ou "" si pas concerné)
+  available: "sept 2026/Oct 2026",     // date de disponibilité
+  avatar: "/avatars/robin_bouvier.jpg",  // chemin vers ta photo (étape 2)
+  stack: [
+    { name: "HTML5",   icon: "devicon-html5-plain" },
+    { name: "CSS",   icon: "devicon-css3-plain" },
+    { name: "Python",  icon: "devicon-python-plain" },
+    { name: "Docker",  icon: "devicon-docker-plain" },
+    { name: "C",  icon: "devicon-c-plain" },
+    { name: "Git",  icon: "devicon-git-plain" },
+    { name: "Bash",  icon: "devicon-bash-plain" },
+    { name: "Linux",  icon: "devicon-linux-plain" },
+    { name: "Ansible", icon: "devicon-ansible-plain" },
+    { name: "Terraform", icon: "devicon-terraform-plain" },
+    { name: "Prometheus", icon: "devicon-prometheus-original" },
+    { name: "MySQL", icon: "devicon-mysql-original" },
+    // ajoute autant de technos que tu veux
+  ],
+  quote: "Mon mot de passe est tellement sécurisé que même moi, je ne le retrouve plus..",
+  links: {
+    linkedin: "www.linkedin.com/in/robin-bouvier",
+    github: "https://github.com/Robin73389",
+  },
+},
+  {
+    name: "Ilan D.",           // ton prénom + initiale du nom
+    accent: "#ff4500",           // couleur de ton profil (voir palette ci-dessous)
+    search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Dev Fullstack",       // ton intitulé de poste
+    rhythm: "4j / 1j",   // rythme d'alternance (ou "" si pas concerné)
+    available: "Aout / Septembre / Novembre 2026",     // date de disponibilité
+    avatar: "/avatars/ilan_deversenne.png",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "React",   icon: "devicon-react-original" },
+      { name: "Python",  icon: "devicon-python-plain" },
+      { name: "Docker",  icon: "devicon-docker-plain" },
+      { name: "NodeJS", icon: "devicon-nodejs-plain" },
+      { name: "HTML5", icon: "devicon-html5-plain" },
+      { name: "CSS3", icon: "devicon-css3-plain" },
+      { name: "Tailwind", icon: "devicon-tailwindcss-original" },
+      { name: "Figma", icon: "devicon-figma-plain" },
+      { name: "Git", icon: "devicon-git-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Bash", icon: "devicon-bash-plain" },
+      { name: "MySQL", icon: "devicon-mysql-plain" }
+    ],
+    quote: "Je construis des trucs qui marchent. Parlons-en.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/ilan-deversenne",
+      github: "https://github.com/ilan-deversenne",
+    },
+  },
+{
+    name: "Sébastien L.",           // ton prénom + initiale du nom
+    accent: "#ff6a00",           // couleur de ton profil (voir palette ci-dessous)
+    search: "CDI,CDD",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+    role: "Dev Front-End",       // ton intitulé de poste
+    rhythm: "Now",   // rythme d'alternance (ou "" si pas concerné)
+    available: "jui. 2026",     // date de disponibilité
+    avatar: "/avatars/lamblin_sebastien.jpg",  // chemin vers ta photo (étape 2)
+    stack: [
+      { name: "C", icon: "devicon-c-plain" },
+      { name: "JavaScript", icon: "devicon-javascript-plain" },
+      { name: "Python", icon: "devicon-python-plain" },
+      { name: "Java", icon: "devicon-java-plain-wordmark" },
+      { name: "MySQL", icon: "devicon-mysql-plain" },
+      { name: "HTML5", icon: "devicon-html5-plain" },
+      { name: "CSS3", icon: "devicon-css3-plain" },
+      { name: "Linux", icon: "devicon-linux-plain" },
+      { name: "Bash", icon: "devicon-bash-plain" },
+      { name: "Git", icon: "devicon-git-plain" }
+
+      // ajoute autant de technos que tu veux
+    ],
+    quote: "Je fais parler les pixels et obéir le code.",
+    links: {
+      linkedin: "https://www.linkedin.com/in/snln/",
+      github: "https://github.com/S3333B",
+    },
+  },
+  {
+  name: "Lonie M.",           // ton prénom + initiale du nom
+  accent: "#ff6a00",           // couleur de ton profil (voir palette ci-dessous)
+  search: "Alternance",        // type de contrat recherché : "Alternance", "Stage", "CDI"
+  role: "Cybersecurité , DevOps",       // ton intitulé de poste
+  rhythm: "4 j. / 1 j.",   // rythme d'alternance (ou "" si pas concerné)
+  available: "sept. 2026",     // date de disponibilité
+  avatar: "/avatars/lonie_meite.png",  // chemin vers ta photo (étape 2)
+  stack: [
+    { name: "C",   icon: "devicon-c-plain" },
+    { name: "Python",  icon: "devicon-python-plain" },
+    { name: "Docker",  icon: "devicon-docker-plain" },
+	{ name: "Javascript", icon: "devicon-javascript-plain" },
+	{ name: "Git", icon: "devicon-git-plain" },
+	{ name: "Linux", icon: "devicon-linux-plain" },
+	{ name: "Bash", icon: "devicon-bash-plain" },
+	{ name: "MySQL", icon: "devicon-mysql-plain" },
+    // ajoute autant de technos que tu veux
+  ],
+  quote: "Entre C, Python et MySQL, j’avance avec méthode et beaucoup de persévérance.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/lonie-meite/",
+    github: "https://github.com/12333-debug",
+  },
+},
+
+{
+  name: "Ouarda B.",
+  accent: "#f3217C",
+  search: "Alternance",
+  role: "Dev Fullstack / AI Agentic",
+  rhythm: "4 jour. / 1 jour.",
+  available: "Nov/Dec 2026",
+  avatar: "/avatars/ouarda_bouchema.png",
+  stack: [
+    { name: "C",          icon: "devicon-c-plain" },
+    { name: "Python",     icon: "devicon-python-plain" },
+    { name: "JavaScript", icon: "devicon-javascript-plain" },
+    { name: "HTML5",      icon: "devicon-html5-plain" },
+    { name: "CSS3",       icon: "devicon-css3-plain" },
+    { name: "MySQL",      icon: "devicon-mysql-plain" },
+    { name: "Git",        icon: "devicon-git-plain" },
+  ],
+  quote: "De 10 ans en soins de santé à la tech — je construis des solutions qui ont du sens.",
+  links: {
+    linkedin: "https://www.linkedin.com/in/OuardaBouchema/",
+    github: "https://github.com/OuardaHolberton",
+  },
+},
+
+]
